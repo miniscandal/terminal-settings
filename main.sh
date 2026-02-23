b6e37469d608c1c3bb7b614cbd54eb18b7c82b8c
miniscandal <oscar01dev@gmail.com>
2026-02-23 14:18:31 -0600

    feat(shell): add welcome script with random ascii-art and system branding

---
+++ main.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+eval "$(oh-my-posh init bash --config ~/.cache/oh-my-posh/themes/M365Princess.omp.json)"
+
+echo -e "> Welcome, miniscandal! ⚝\n" | pv -qL 20
+echo -e -n "> No Anime No Life ꉂ(˵˃ ᗜ ˂˵) - No Code No Life ＼_ﾍ(ω｀●)\n\n"
+
+DIR=~/miniscandal/profile/ascii-arts
+CHARACTER_ID=""
+
+# Random ascii-art character
+if [ -z "$CHARACTER_ID" ]; then
+  FILE=$(ls "$DIR"/character-*.txt | shuf -n 1)
+else
+  FILE=$(ls "$DIR"/character-$CHARACTER_ID.txt)
+fi
+
+cat "$FILE" | lolcat -f
+
+figlet "      miniscandal" | lolcat
+
+TEXT_COLOR='\033[0;36m'
+NC='\033[0m'
+
+paste \
+  <(catimg ~/miniscandal/pictures/sticker-dango-01.png -w 100 && echo -e "$TEXT_COLOR   Anime$NC") \
+  <(catimg ~/miniscandal/pictures/sticker-dango-02.png -w 100 && echo -e "  ${TEXT_COLOR} Backend$NC") \
+  <(catimg ~/miniscandal/pictures/sticker-dango-03.png -w 100 && echo -e "  ${TEXT_COLOR}Assembly$NC") \
+  <(catimg ~/miniscandal/pictures/sticker-dango-04.png -w 100 && echo -e "  ${TEXT_COLOR}Frontend$NC") \
+  <(catimg ~/miniscandal/pictures/sticker-dango-05.png -w 100 && echo -e " $TEXT_COLOR  K-pop$NC")
+echo -e "\n"

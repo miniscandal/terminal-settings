674eb49d0b45241a1e9dbb961e6db1f2a47dec6a
miniscandal <oscar01dev@gmail.com>
2026-03-03 15:11:53 -0600

    feat(bash): enhance main profile with system info and path exports
    - Add OS_RELEASE detection from /etc/os-release.
    - Display user and OS info on startup.
    - Export ~/.local/bin to PATH.
    - Configure OLLAMA_HOST to use the WSL gateway IP.

--- src/bash/main.sh
+++ src/bash/main.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+OS_RELEASE=$(grep '^PRETTY_NAME=' /etc/os-release | cut -d'=' -f2 | tr -d '"')
+
 echo -e "> Welcome, miniscandal! ⚝\n" | pv -qL 10
+echo -e "> User: $USER | OS Release: $OS_RELEASE ☾\n"
 echo -e -n "> No Anime No Life ꉂ(˵˃ ᗜ ˂˵) - No Code No Life ＼_ﾍ(ω｀●)\n\n"
 
 PROFILE_DIR=~/miniscandal/profile
@@ -32,6 +35,7 @@ paste \
   <(catimg ${DIR_STICKERS}/dango-05.png -w 100 && echo -e " $TEXT_COLOR  K-pop$NC")
 echo -e "\n"
 
+export PATH="$PATH:$HOME/.local/bin"
 THEME_FILE=~/miniscandal/profile/src/assets/oh-my-posh/themes/catppuccin.omp.json
 eval "$(oh-my-posh init bash --config $THEME_FILE)"
 
@@ -46,3 +50,5 @@ else
   # Fallback: ensures 'ls' remains functional and colored
   alias ls='ls --color=auto'
 fi
+
+export OLLAMA_HOST=$(ip route show default | awk '{print $3}'):11434
